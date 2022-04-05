37cc2f2a01cbb60aa6c21cee5338cfe4ec92e040
Your Name <100946724+ryodmm@users.noreply.github.com>
2022-04-05 04:54:52 +0000

    remove_ruby

---
+++ lesson6-2.rb
@@ -0,0 +1,28 @@
+puts "計算をはじめます"
+puts "何回計算を繰り返しますか"
+n = gets.to_i
+puts = "{n}"
+
+for i in 1..n do
+  puts "#{i}回目の計算"
+
+puts
+
+puts "2つの値を入力してください"
+a = gets.to_i
+puts = "{a}"
+b = gets.to_i
+puts = "{b}"
+
+puts "a=" "#{a}"
+puts "b=" "#{b}"
+puts "計算結果を出力します"
+puts "a+b=" "#{a+b}"
+puts "a-b=" "#{a-b}"
+puts "a*b=" "#{a*b}"
+puts "a/b=" "#{a/b}"
+
+puts "計算を終了します"
+
+end
+
